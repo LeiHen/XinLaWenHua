d0698fad20ede81d931f7405eae25ad79af49b53
Nice <61955365@qq.com>
2016-09-28 18:55:33 +0800

    no message

---
+++ cms2/common.inc.php
@@ -0,0 +1,9 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'dbmxinlawenhua';
+$cfg_dbuser = 'dbmxinlawenhua';
+$cfg_dbpwd = 'mxenanews@db';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+?>
